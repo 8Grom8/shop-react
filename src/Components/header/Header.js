dd36ec371a06204d7971dd245a1de2a867eff871
Michal Lenyuk <mlcorp@ukr.net>
2021-06-07 20:00:10 +0300

    practice-1

---
+++ src/Components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Headerlist from './headerlist/Headerlist';
+const Header = () => {
+    return (
+        <header>
+            <Headerlist />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/Components/main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import data from '../../data';
+import PhoneList from '../phoneList/PhoneList';
+import LaptopList from "../laptopList/LaptopList";
+
+const Main = () => {
+    return (
+      <main>
+        <PhoneList phones={data.phones} />
+        <LaptopList laptops={data.laptops} />
+      </main>
+    );
+}
+
+export default Main;
